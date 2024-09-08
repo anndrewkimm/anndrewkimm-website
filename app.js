33d728418b268a891e7bafd9c7268ff685864a0f
anndrewkimm <akim9165@gmail.com>
2024-09-07 17:12:19 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+// You can add interactivity or effects here.
+// Example: Log a message when the page loads
+window.onload = function () {
+    console.log("Welcome to Andrew Kim's CS portfolio!");
+  };
